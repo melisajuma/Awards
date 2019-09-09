4c424df6bb2b92b2a364fdb72d35e760d23147a7
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 05:29:12 +0300

    make file imports

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import logout
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.http import Http404
 
 # Create your views here.
+
+
