be828e0899c8bc125898cda16b78d1c6abf10654
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:53:03 +0300

    make file imports

--- awards/serilizer.py
+++ awards/serilizer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import *
+
+class ProjSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('project_name', 'project_photo', 'description', 'github_repo', 'url',)
