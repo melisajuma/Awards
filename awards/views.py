2bf84fc9937ff4754e496c0c9affff5d0a67cb5f
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 05:33:32 +0300

    create register function

--- awards/views.py
+++ awards/views.py
@@ -7,4 +7,12 @@ from django.http import Http404
 
 # Create your views here.
 
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', locals())
