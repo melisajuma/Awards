aab51199b93aff9483f0dec6caf88ed1fbf8be9e
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:12:35 +0300

    create additional file in the app

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Projects, Profile
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Projects)
