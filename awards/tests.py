f0835e2932c7493d9bf69c56e4e130b89bcf6e7d
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:57:37 +0300

    create a TestCase class

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 from .models import *
 # Create your tests here.
+
+class ProjectsTestCase(TestCase):
+    def setUp(self, project_name='GitSearch', project_photo='lol.jpg', description='some description', github_repo='git repo', url='lol.com', owner='Mercurial'):
+        return Projects.objects.create(project_name=project_name, project_photo=project_photo, description=description, github_repo=github_repo, url=url, owner=owner)
+
+    def projectSave(self):
+        initialization = self.setUp()
+        self.assertTrue(save > 0)
+
