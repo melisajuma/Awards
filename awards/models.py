e1d77f8ec1a9e6126ea312c831769d5ddeaf30d3
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:48:53 +0300

    make changes to app folder

--- awards/models.py
+++ awards/models.py
@@ -4,3 +4,41 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profile/')
+    bio = models.TextField(blank=True)
+    user_id = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+    return self.bio
+
+    def save_user(self):
+    self.save()
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.description
+
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=50, blank=True)
+    project_photo = models.ImageField(upload_to='projectpics/')
+        description = models.TextField(max_length=600, blank=True)
+    github_repo = models.CharField(max_length=200, blank=True)
+    url = models.CharField(max_length=50, blank=True)
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
