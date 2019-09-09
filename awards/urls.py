0eafdc80190a08886bff9e68f26fee1158a9a42a
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:59:28 +0300

    add url patterns

--- awards/urls.py
+++ awards/urls.py
@@ -3,3 +3,14 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf.urls import url,include
 from django.conf import settings
+
+urlpatterns=[
+    url(r'^$', views.register, name='register'),
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^home/$',views.home, name='home'),
+    url(r'^project/(?P<project_id>[0-9])$',views.project,name ='project'),
+    url(r'profile/',views.profile, name='profile'),
+    url(r'^post/', views.upload_form, name='post'),
+    url(r'^edit/', views.edit_prof, name='edit'),
+
+]
