de13476fc85822641974ea25a791bfaf86486c3f
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:16:17 +0300

    add form classes

--- awards/forms.py
+++ awards/forms.py
@@ -1,2 +1,13 @@
 from django import forms
 from .models import Projects, Profile
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_photo','bio')
+
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        fields = ('project_name','project_photo','description','url','owner')
