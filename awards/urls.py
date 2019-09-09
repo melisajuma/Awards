ebc23a13c3f9eb9108bb2ebf1486719b676538fc
melisajuma <Melisaakinyi95@gmail.com>
2019-09-09 04:58:00 +0300

    make file imports

---
+++ awards/urls.py
@@ -0,0 +1,5 @@
+from awewards import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf.urls import url,include
+from django.conf import settings
