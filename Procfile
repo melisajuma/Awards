ab8826d22a28f94cccd90c62ce51c9b19e51ca5e
melisajuma <Melisaakinyi95@gmail.com>
2019-09-10 16:03:31 +0300

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn onlyawards.wsgi --log-file -
--- onlyawards/settings.py
+++ onlyawards/settings.py
@@ -94,11 +94,44 @@ WSGI_APPLICATION = 'onlyawards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
     }
+       
+}
+# production
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+    )
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME':'awards',
+            'USER': 'moringa',
+            'PASSWORD':'123',
+            'HOST': 'localhost',
+            'PORT': '',
+    }       
 }
 
 
